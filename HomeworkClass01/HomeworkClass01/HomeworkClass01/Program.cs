e654b01ef72b9a275ff39a88a1093639a10d00e6
JaneAndonovski <jane.andonovski@yahoo.com>
2020-03-11 20:02:30 +0100

    homeworkclass01

---
+++ HomeworkClass01/HomeworkClass01/HomeworkClass01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace SEDC.Class01.Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Traffic light...");
+            Thread.Sleep(4000);
+
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red light - STOP",
+                              Console.BackgroundColor);
+            Thread.Sleep(4000);
+
+
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" ",
+                                Console.BackgroundColor);
+
+
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Yellow light - PREPARE.",
+                              Console.BackgroundColor);
+            Thread.Sleep(4000);
+
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" ",
+                              Console.BackgroundColor);
+
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green light - GO",
+                                Console.BackgroundColor);
+            Thread.Sleep(4000);
+
+            Console.ReadLine();
+        }
+    }
+}
